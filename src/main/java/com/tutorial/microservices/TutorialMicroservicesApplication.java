25dc82720e4a7938695883e19bec01d302343bdf
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-16 09:10:29 +0700

    rename application main class

--- src/main/java/com/tutorial/microservices/TutorialMicroservicesApplication.java
+++ src/main/java/com/tutorial/microservices/TutorialMicroservicesApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.tutorial.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class TutorialMicroservicesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(TutorialMicroservicesApplication.class, args);
 	}
 
 }
--- src/test/java/com/tutorial/microservices/TutorialMicroservicesApplicationTests.java
+++ src/test/java/com/tutorial/microservices/TutorialMicroservicesApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.tutorial.microservices;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class TutorialMicroservicesApplicationTests {
 
 	@Test
 	void contextLoads() {
